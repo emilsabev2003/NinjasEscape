eded47b2d56b021e1abb0b9d9bfaaa1a9e788d7e
emilsabev2003 <150591482+emilsabev2003@users.noreply.github.com>
2024-02-14 09:50:26 -0800

    setting up folders/files

---
+++ src/scenes/Menu.js
@@ -0,0 +1,24 @@
+class Menu extends Phaser.Scene {
+    constructor() 
+    {
+        super('menuScene')
+    }
+
+    preload() //load images, audio, sprites here
+    {
+      
+
+    }
+
+    create() //add custom background, animation configuration, display menu text and instructions, define keys
+    {
+        
+
+    }
+
+    update() //transition from menu to play if key is pressed
+    {
+        
+
+    }
+}
---
+++ src/scenes/Play.js
@@ -0,0 +1,16 @@
+class Play extends Phaser.Scene {
+    constructor() {
+        super('playScene')
+    }
+
+    create() //add music, background, sprites, define keys, initialize/display score, game over flag
+    {
+        
+    }
+
+    update() //check collisions, game over, movement/animation, 
+    {
+    
+    }
+
+}
