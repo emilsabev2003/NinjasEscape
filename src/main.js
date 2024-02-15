81b77bc3be0bcd3c25a4e3c2d358c29710b9d395
emilsabev2003 <150591482+emilsabev2003@users.noreply.github.com>
2024-02-14 23:54:42 -0800

    final with comments

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 // Ninjas Escape
 // Name: Emil Sabev
 // Date: 2/14/2024
+// Approx Hours Spent: 13
 
 //credits: https://pixabay.com/music/crime-scene-the-big-heist-188391/
 //         https://www.fesliyanstudios.com/royalty-free-sound-effects-download/whoosh-and-swoosh-73
 //         https://pixabay.com/sound-effects/medium-explosion-40472/
+
+/*
+This is the first time I've ever used a pixel art editor, and while the programming itself may not be that great, I am still proud of the animations 
+I was able to draw for the ninja. It was very difficult figuring out how to format the pngs and atlas together but the end result felt very satisfying.
+*/
 
 'use strict'
 
